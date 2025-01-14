9b34ae677d82d26bc52a44b5d92d9b72fc182186
Donvicio <10143@portalcedi.edu.mx>
2025-01-14 10:10:27 -0600

    formulario

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: false,
 })
 export class HomePage {
 
-  constructor() {}
+  nombre = new FormControl('');
+
+  constructor() {
+  }
 
 }
